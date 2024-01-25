be4eb9295444516738297de4f816b4c29dae1048
hyungji <icthelight@naver.com>
2024-01-25 22:42:33 +0900

    🔧 chore: pandacss config

--- packages/ark/src/index.tsx
+++ packages/ark/src/index.tsx
@@ -5,7 +5,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-import "./index.css";
+import "./styled-system/styles.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
