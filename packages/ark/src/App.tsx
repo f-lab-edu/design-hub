cb8fd20437f35353bf7adfaaba7ee52779508810
hyungji <icthelight@naver.com>
2024-01-26 01:13:59 +0900

    #8 💄 feat: ark/slider

--- packages/ark/src/App.tsx
+++ packages/ark/src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { Slider } from "@ark-ui/react";
+
 import { css } from "./styled-system/css";
 
 import "./App.css";
@@ -10,6 +12,17 @@ function App() {
       <span className={css({ color: "red.300", bg: "yellow.100" })}>악</span>
       Hello 🐼!
       <p className={css({ color: "blue.300" })}>드디어 성공...</p>
+      <Slider.Root>
+        <Slider.Label className={css({ color: "yellow.500" })}>
+          Label
+        </Slider.Label>
+        <Slider.Control>
+          <Slider.Track>
+            <Slider.Range />
+          </Slider.Track>
+          <Slider.Thumb key={0} index={0} />
+        </Slider.Control>
+      </Slider.Root>
     </div>
   );
 }
