f1081afb0d377f6aec808bdfc4385eb67f58c367
hyungji <icthelight@naver.com>
2024-01-31 00:53:01 +0900

    🏷️  #23 feat: polymorphic types

---
+++ packages/hj-design-system/components/polymorphic.d.ts
@@ -0,0 +1,17 @@
+import {
+  type ComponentPropsWithoutRef,
+  type ElementType,
+  type PropsWithChildren,
+} from "react";
+
+type AsProp<C extends ElementType> = {
+  as?: C;
+};
+
+type PropsToOmit<C extends ElementType, P> = keyof (AsProp<C> & P);
+
+export type PolymorphicComponentProp<
+  C extends ElementType,
+  Props = object,
+> = PropsWithChildren<Props & AsProp<C>> &
+  Omit<ComponentPropsWithoutRef<C>, PropsToOmit<C, Props>>;
