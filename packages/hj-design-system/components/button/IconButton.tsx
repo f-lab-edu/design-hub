92a267f36d189a1ea020cc9455369db6f15c217a
hyungji <icthelight@naver.com>
2024-02-03 22:29:40 +0900

    ✨ #25 feat: IconButton 컴포넌트 생성

---
+++ packages/hj-design-system/components/button/IconButton.tsx
@@ -0,0 +1,80 @@
+/** @jsxImportSource @emotion/react */
+
+import {
+  cloneElement,
+  type CSSProperties,
+  type ElementType,
+  forwardRef,
+  isValidElement,
+  type ReactElement,
+} from "react";
+
+import {
+  type PolymorphicComponentPropsWithRef,
+  type PolymorphicRef,
+} from "components/polymorphic";
+
+import Button from "./Button";
+import {
+  type ButtonColorScheme,
+  type ButtonSizeSet,
+  type ButtonVariants,
+} from "./types";
+
+type IconButtonProps<C extends ElementType = "button"> =
+  PolymorphicComponentPropsWithRef<
+    C,
+    {
+      /**
+       * The label for the button.
+       */
+      "aria-label": string;
+      /**
+       * The icon to be used in the button.
+       * @type ReactElement
+       */
+      icon?: ReactElement;
+      /**
+       * The size of the icon.
+       * @default md
+       */
+      size?: ButtonSizeSet;
+      /**
+       * The color scheme of the icon.
+       * @default blue
+       */
+      colorScheme?: ButtonColorScheme;
+      /**
+       * The variant of the icon.
+       * @default solid
+       */
+      variant?: ButtonVariants;
+      /**
+       * The styles of the icon.
+       */
+      styles?: CSSProperties;
+    }
+  >;
+
+const IconButton = forwardRef(function IconButton<
+  C extends ElementType = "button",
+>(props: IconButtonProps<C>, ref?: PolymorphicRef<C>) {
+  const { icon, children, "aria-label": ariaLabel, ...rest } = props;
+
+  const element = icon || children;
+
+  const _children = isValidElement(element)
+    ? cloneElement(element as any, {
+        "aria-hidden": true,
+        focusable: false,
+      })
+    : null;
+
+  return (
+    <Button ref={ref} aria-label={ariaLabel} {...rest}>
+      {_children}
+    </Button>
+  );
+});
+
+export default IconButton;
