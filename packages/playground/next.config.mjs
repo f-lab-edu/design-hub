9e8467a6367a1364304e27c554b6e59fa97eca21
hyungji <icthelight@naver.com>
2024-02-03 14:11:48 +0900

    🙈 chore: playground 패키지 git 추적하지 않도록

--- packages/playground/next.config.mjs
+++
@@ -1,9 +0,0 @@
-import withPlugins from "next-compose-plugins";
-import withTM from "next-transpile-modules";
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
-
-export default withPlugins([withTM(["@hub/hds"])], nextConfig);
