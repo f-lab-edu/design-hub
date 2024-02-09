19d17e11184f2d1def2444954ccaa8cd465bd102
hyungji <icthelight@naver.com>
2024-02-09 23:10:15 +0900

    💄 feat: input-root 에 스타일 적용

--- packages/hj-design-system/components/input/input-root.tsx
+++ packages/hj-design-system/components/input/input-root.tsx
@@ -1,9 +1,13 @@
+/** @jsxImportSource @emotion/react */
+
 import { type ElementType, forwardRef } from "react";
 
 import {
   type PolymorphicComponentProp,
   type PolymorphicRef,
 } from "components/polymorphic";
+
+import { baseStyle } from "./styles";
 
 type InputRootProps<C extends ElementType> = PolymorphicComponentProp<C>;
 
@@ -12,7 +16,7 @@ const InputRoot = forwardRef(function InputRoot<C extends ElementType = "div">(
   ref?: PolymorphicRef<C>,
 ) {
   const Component = props.as || "div";
-  return <Component ref={ref} {...props} />;
+  return <Component ref={ref} css={baseStyle} {...props} />;
 });
 
 export default InputRoot;
---
+++ packages/hj-design-system/components/input/styles/input-root.ts
@@ -0,0 +1,8 @@
+import { css } from "@emotion/react";
+
+export const baseStyle = css({
+  position: "relative",
+  display: "flex",
+  alignItems: "center",
+  width: "100%",
+});
