8f7412721f26da42be8641ed3c8879befeefb752
hyungji <icthelight@naver.com>
2024-02-09 23:09:16 +0900

    ✨ feat: Input 컴포넌트 as prop 추가, size 에 따른 스타일 적용

--- packages/hj-design-system/components/input/input-input.tsx
+++ packages/hj-design-system/components/input/input-input.tsx
@@ -1,17 +1,35 @@
-import { type ElementType, forwardRef } from "react";
+/** @jsxImportSource @emotion/react */
 
+import { type ElementType, forwardRef, type InputHTMLAttributes } from "react";
+
+import { css } from "@emotion/react";
 import {
-  type PolymorphicComponentProp,
+  type PolymorphicComponentPropsWithRef,
   type PolymorphicRef,
 } from "components/polymorphic";
 
-type InputProps<C extends ElementType> = PolymorphicComponentProp<C>;
+import { baseStyle, getSizeStyles } from "./styles";
+import { type InputSizeSet } from "./types";
+
+type CustomInputProps = Omit<InputHTMLAttributes<HTMLInputElement>, "size"> & {
+  inputSize?: InputSizeSet;
+};
+
+type InputProps<C extends ElementType = "input"> = CustomInputProps &
+  PolymorphicComponentPropsWithRef<C>;
 
 const InputInput = forwardRef(function InputInput<
   C extends ElementType = "input",
 >(props: InputProps<C>, ref?: PolymorphicRef<C>) {
-  const Component = props.as || "input";
-  return <Component ref={ref} {...props} />;
+  const { size: inputSize } = props;
+
+  return (
+    <input
+      ref={ref}
+      css={css(baseStyle, getSizeStyles(inputSize))}
+      {...props}
+    />
+  );
 });
 
 export default InputInput;
