60ad9ceca359483daf685de0058fbf0afb70f286
hyungji <icthelight@naver.com>
2024-02-10 23:16:26 +0900

    💄 feat: input-addon 스타일 추가

--- packages/hj-design-system/components/input/input-addon-after.tsx
+++ packages/hj-design-system/components/input/input-addon-after.tsx
@@ -1,12 +1,15 @@
 /** @jsxImportSource @emotion/react */
 
-import { type ElementType, forwardRef } from "react";
+import { type ElementType, forwardRef, useContext, useMemo } from "react";
 
 import { css } from "@emotion/react";
 import {
   type PolymorphicComponentPropsWithRef,
   type PolymorphicRef,
 } from "components/polymorphic";
+
+import { getAddonSizeStyles, inputAddonBaseStyle } from "./styles/input-addon";
+import { InputContext } from "./input-context";
 
 type InputAddonAfterProps<C extends ElementType = "div"> =
   PolymorphicComponentPropsWithRef<C>;
@@ -17,15 +20,23 @@ const InputAddonAfter = forwardRef(function InputAddonAfter<
   const { as, style, ...rest } = props;
   const Component = as || "div";
 
-  const addonBaseStyle = css({
-    flex: "0 0 auto",
-    width: "auto",
-    display: "flex",
-    alignItems: "center",
-    whiteSpace: "nowrap",
+  const inputContext = useContext(InputContext);
+
+  const addonBeforeStyle = css({
+    borderRadius: "0 6px 6px 0",
   });
 
-  return <Component css={css(addonBaseStyle, style)} ref={ref} {...rest} />;
+  const styles = useMemo(
+    () => [
+      inputAddonBaseStyle,
+      addonBeforeStyle,
+      getAddonSizeStyles(inputContext?.size),
+      style,
+    ],
+    [style, inputContext?.size]
+  );
+
+  return <Component css={styles} ref={ref} {...rest} />;
 });
 
 export default InputAddonAfter;
