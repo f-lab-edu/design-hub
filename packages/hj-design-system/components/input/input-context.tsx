9dc3908eebac6ae9939406c7e0f4edc74f201fde
hyungji <icthelight@naver.com>
2024-02-11 01:08:24 +0900

    ✨ feat: input-context 에 affix, addon 을 가졌는지 여부 추가

--- packages/hj-design-system/components/input/input-context.tsx
+++ packages/hj-design-system/components/input/input-context.tsx
@@ -1,10 +1,27 @@
-import { createContext, type ReactNode, useMemo, useState } from "react";
+import {
+  createContext,
+  type Dispatch,
+  type ReactNode,
+  type SetStateAction,
+  useMemo,
+  useState,
+} from "react";
 
-import { type InputSizeSet } from "./types";
+import { type InputSizeSet, type InputVariant } from "./types";
 
 interface InputContextType {
   size: InputSizeSet;
-  setSize: (size: InputSizeSet) => void;
+  setSize: Dispatch<SetStateAction<InputSizeSet>>;
+  variant: InputVariant;
+  setVariant: Dispatch<SetStateAction<InputVariant>>;
+  hasPrefix: boolean;
+  setHasPrefix: Dispatch<SetStateAction<boolean>>;
+  hasSuffix: boolean;
+  setHasSuffix: Dispatch<SetStateAction<boolean>>;
+  hasAddonBefore: boolean;
+  setHasAddonBefore: Dispatch<SetStateAction<boolean>>;
+  hasAddonAfter: boolean;
+  setHasAddonAfter: Dispatch<SetStateAction<boolean>>;
 }
 
 export const InputContext = createContext<InputContextType | null>(null);
@@ -12,7 +29,33 @@ export const InputContext = createContext<InputContextType | null>(null);
 const InputProvider = ({ children }: { children: ReactNode }) => {
   const [size, setSize] = useState<InputSizeSet>("md");
 
-  const ContextValue = useMemo(() => ({ size, setSize }), [size]);
+  const [variant, setVariant] = useState<InputVariant>("outline");
+
+  const [hasPrefix, setHasPrefix] = useState<boolean>(false);
+
+  const [hasSuffix, setHasSuffix] = useState<boolean>(false);
+
+  const [hasAddonBefore, setHasAddonBefore] = useState<boolean>(false);
+
+  const [hasAddonAfter, setHasAddonAfter] = useState<boolean>(false);
+
+  const ContextValue = useMemo(
+    () => ({
+      size,
+      setSize,
+      variant,
+      setVariant,
+      hasPrefix,
+      setHasPrefix,
+      hasSuffix,
+      setHasSuffix,
+      hasAddonBefore,
+      setHasAddonBefore,
+      hasAddonAfter,
+      setHasAddonAfter,
+    }),
+    [size, variant, hasPrefix, hasSuffix, hasAddonBefore, hasAddonAfter]
+  );
 
   return (
     <InputContext.Provider value={ContextValue}>
