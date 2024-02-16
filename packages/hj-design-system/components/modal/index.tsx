e1ac5700835a285040d4d893da3358ae9209b3ec
hyungji <icthelight@naver.com>
2024-02-16 21:34:44 +0900

    ✨ feat: modal-footer 컴포넌트 생성

--- packages/hj-design-system/components/modal/index.tsx
+++ packages/hj-design-system/components/modal/index.tsx
@@ -1,7 +1,9 @@
 import { ModalHeader as Header } from "./modal-header";
 import { ModalBody as Body } from "./modal-body";
+import { ModalFooter as Footer } from "./modal-footer";
 
 export const Modal = {
   Header,
   Body,
+  Footer,
 };
