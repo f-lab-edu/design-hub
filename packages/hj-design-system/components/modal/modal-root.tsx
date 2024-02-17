bbfb20d2aefca177a068835f0845db118d5fee39
hyungji <icthelight@naver.com>
2024-02-17 18:45:31 +0900

    ✨ portal 과 결합도를 낮춘 modal-root

--- packages/hj-design-system/components/modal/modal-root.tsx
+++ packages/hj-design-system/components/modal/modal-root.tsx
@@ -2,20 +2,38 @@ import {
   PolymorphicComponentPropsWithRef,
   PolymorphicRef,
 } from "components/polymorphic";
-import { ElementType, forwardRef } from "react";
+import { ElementType, ReactElement, ReactNode, forwardRef } from "react";
+import { useScrollLock } from "../../hooks/use-scroll-lock";
 
 type ModalRootProps<C extends ElementType = "div"> =
-  PolymorphicComponentPropsWithRef<C>;
+  PolymorphicComponentPropsWithRef<
+    C,
+    {
+      /**
+       * If `true`, the modal will be open.
+       */
+      isOpen: boolean;
+      /**
+       * The wrapper element to render the modal into.
+       * @default component/Portal
+       */
+      wrapper?: (children: ReactNode) => ReactElement;
+    }
+  >;
 
 export const ModalRoot = forwardRef(function ModalRoot<
   C extends ElementType = "div",
 >(props: ModalRootProps<C>, ref?: PolymorphicRef<C>) {
-  const { as, children, ...rest } = props;
+  const { as, children, isOpen, wrapper, ...rest } = props;
   const Component = as || "div";
 
-  return (
-    <Component ref={ref} {...rest}>
+  useScrollLock(isOpen);
+
+  const modalRoot = (
+    <Component role="dialog" ref={ref} {...rest}>
       {children}
     </Component>
   );
+
+  return wrapper ? wrapper(modalRoot) : modalRoot;
 });
