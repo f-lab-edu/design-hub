d512878c93be203a1c5125366bc74c5b00ee2a6b
hyungji <icthelight@naver.com>
2024-02-17 22:23:05 +0900

    📝 docs: 사이즈별 모달 스토리 추가

--- packages/hj-design-system/components/modal/stories/modal.stories.tsx
+++ packages/hj-design-system/components/modal/stories/modal.stories.tsx
@@ -1,48 +1,30 @@
 import { useState } from "react";
 import { Modal } from "../";
 import Button from "components/button/Button";
+import { ModalSizeSet } from "../types";
 
 export default {
   title: "Components/Modal",
   component: Modal,
+  argTypes: {
+    size: {
+      control: "select",
+      options: ["xs", "sm", "md", "lg"],
+    },
+  },
 };
 
-export const DefaultModal = () => {
+const Template = ({ size }: { size: ModalSizeSet }) => {
   const [isOpen, setIsOpen] = useState(false);
+
   return (
     <>
       <Button onClick={() => setIsOpen(true)}>Open Modal</Button>
       <Modal.Root isOpen={isOpen} onClose={() => setIsOpen(false)}>
-        <Modal.Content>
+        <Modal.Content size={size}>
           <Modal.Header>Modal Header</Modal.Header>
           <Modal.Body>
             <p>Modal Body</p>
-            <span>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod
-              minima nihil corrupti minus! Eveniet reiciendis mollitia ab,
-              deserunt quasi tempore hic consectetur temporibus rem explicabo
-              aperiam odio? Provident, consequuntur nemo iusto quas, quasi
-              pariatur tenetur natus aspernatur non id quo iure dicta dolorem
-              labore quaerat voluptatibus ratione repellat ab veniam voluptatum
-              a earum illo. Reprehenderit, esse autem debitis quidem aperiam nam
-              earum libero vero. Dignissimos similique sequi rem eius nisi ab
-              repellat ipsa voluptatum id itaque ipsam, neque voluptate quo
-              officia? Quasi placeat, et cum ipsam iusto ullam obcaecati quia
-              accusamus natus dignissimos autem corrupti necessitatibus odit
-              fugiat voluptatibus dolorum. Nulla temporibus dolore minus, omnis
-              earum sapiente ullam quae accusantium expedita vero odio aliquid
-              soluta? Illo officia magni expedita itaque consequuntur dolorem!
-              Labore, beatae? Minus, ab numquam dolorum exercitationem expedita
-              laboriosam nisi nostrum tempora quas tenetur perferendis aliquam
-              molestiae unde eaque ullam suscipit. Perferendis, voluptas
-              temporibus vel repudiandae quo distinctio omnis unde repellendus
-              hic aspernatur autem illum recusandae! Reiciendis, laudantium?
-              Laudantium blanditiis necessitatibus quod. At enim eligendi soluta
-              ut in aperiam quo alias illum quos porro, cumque ducimus nisi
-              animi! Eveniet at ducimus, exercitationem facilis voluptas rerum
-              dolor dolorem nulla mollitia cum, adipisci totam nam voluptate
-              nesciunt illum nobis eligendi.
-            </span>
           </Modal.Body>
           <Modal.Footer>
             <Button size="sm" colorScheme="green">
@@ -61,3 +43,5 @@ export const DefaultModal = () => {
     </>
   );
 };
+
+export const Size = Template.bind({});
