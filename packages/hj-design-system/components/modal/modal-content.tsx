6d652d15deae053007f547bb02e0419e2656da4b
hyungji <icthelight@naver.com>
2024-02-17 20:59:56 +0900

    💄 feat: modal-content size 별 style 추가

--- packages/hj-design-system/components/modal/modal-content.tsx
+++ packages/hj-design-system/components/modal/modal-content.tsx
@@ -1,20 +1,38 @@
+/** @jsxImportSource @emotion/react */
+
 import {
   PolymorphicComponentPropsWithRef,
   PolymorphicRef,
 } from "components/polymorphic";
-import { ElementType, forwardRef } from "react";
+import { ElementType, forwardRef, useContext, useEffect } from "react";
+import { getSizeStyles, modalContentBaseStyle } from "./styles/modal-content";
+import { ModalSizeSet } from "./types";
+import { ModalContext } from "./modal-context";
 
 type ModalContentProps<C extends ElementType = "div"> =
-  PolymorphicComponentPropsWithRef<C>;
+  PolymorphicComponentPropsWithRef<
+    C,
+    {
+      size?: ModalSizeSet;
+    }
+  >;
 
 export const ModalContent = forwardRef(function ModalContent<
   C extends ElementType = "div",
 >(props: ModalContentProps<C>, ref?: PolymorphicRef<C>) {
-  const { as, children, ...rest } = props;
+  const { as, children, size = "md", ...rest } = props;
   const Component = as || "div";
 
+  const modalContentStyles = [modalContentBaseStyle, getSizeStyles(size)];
+
+  const modalContext = useContext(ModalContext);
+
+  useEffect(() => {
+    modalContext?.setSize(size);
+  }, [size]);
+
   return (
-    <Component ref={ref} {...rest}>
+    <Component ref={ref} css={modalContentStyles} {...rest}>
       {children}
     </Component>
   );
