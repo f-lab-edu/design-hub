326434ad5fb3f98faa2540e86a7c6bd1b6b01484
hyungji <icthelight@naver.com>
2024-02-17 17:57:26 +0900

    ✨ feat: portal 유연성 높이기 container 전달할 수 있도록, portal 을 비활성화할 수 있도록

--- packages/hj-design-system/components/portal/portal.tsx
+++ packages/hj-design-system/components/portal/portal.tsx
@@ -1,16 +1,58 @@
-import { FC, ReactNode, useEffect, useState } from "react";
+import {
+  FC,
+  ForwardedRef,
+  PropsWithChildren,
+  ReactNode,
+  RefObject,
+  cloneElement,
+  forwardRef,
+  isValidElement,
+  useEffect,
+  useState,
+} from "react";
 import { createPortal } from "react-dom";
 
-const Portal: FC<{ children: ReactNode }> = ({ children }) => {
-  const [container, setContainer] = useState<Element | null>(null);
+type PortalContainerType = Element | DocumentFragment;
+
+interface PortalProps {
+  /**
+   * The children to render into the `container`.
+   * @default documnet.body
+   */
+  portalContainer?: Element | DocumentFragment;
+  /**
+   * If `true`, the children will be rendered in place instead of being portaled.
+   */
+  disablePortal?: boolean;
+}
+
+const Portal = forwardRef(function Portal(
+  props: PropsWithChildren<PortalProps>,
+  ref?: ForwardedRef<HTMLElement>
+) {
+  const {
+    children,
+    portalContainer = document.body,
+    disablePortal = false,
+  } = props;
+  const [container, setContainer] =
+    useState<PortalContainerType>(portalContainer);
 
   useEffect(() => {
-    if (document) setContainer(document.body);
-  }, []);
+    if (document) setContainer(portalContainer);
+  }, [portalContainer]);
 
-  if (!container) return null;
+  if (disablePortal) {
+    if (isValidElement(children)) {
+      const newProps = {
+        ref,
+      };
+      return cloneElement(children, newProps);
+    }
+    return children;
+  }
 
-  return createPortal(children, container);
-};
+  return container ? createPortal(children, container) : null;
+});
 
 export default Portal;
