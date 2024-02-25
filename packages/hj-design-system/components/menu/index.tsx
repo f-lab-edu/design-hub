914e965791df548b08ae6cbb730913638ec6b157
hyungji <icthelight@naver.com>
2024-02-25 15:23:51 +0900

    ✨ feat: menu-list 생성

--- packages/hj-design-system/components/menu/index.tsx
+++ packages/hj-design-system/components/menu/index.tsx
@@ -1,7 +1,9 @@
 import { MenuRoot as Root } from "./menu-root";
 import { MenuTrigger as Trigger } from "./menu-trigger";
+import { MenuList as List } from "./menu-list";
 
 export const Menu = {
   Root,
   Trigger,
+  List,
 };
---
+++ packages/hj-design-system/components/menu/menu-list.tsx
@@ -0,0 +1,50 @@
+import { PolymorphicComponentPropsWithRef } from "components/polymorphic";
+import { AnimatePresence, motion } from "framer-motion";
+import { ComponentProps, ElementType, forwardRef, useContext } from "react";
+import { defaultAnimationVariants } from "../../theme/foundations/animation";
+import { MenuContext } from "./menu-context";
+
+type AnimatePresenceProps = ComponentProps<typeof AnimatePresence>;
+
+type MenuListProps<C extends ElementType = "ul"> =
+  PolymorphicComponentPropsWithRef<
+    C,
+    {
+      /**
+       * whether to disable animation
+       * @default false
+       */
+      disableAnimation?: boolean;
+      /**
+       * @description AnimatePresence props
+       */
+      animatePresenceProps?: AnimatePresenceProps;
+    }
+  >;
+
+export const MenuList = forwardRef(function MenuList<
+  C extends ElementType = "ul",
+>(props: MenuListProps<C>) {
+  const { as, children, disableAnimation, ...rest } = props;
+
+  const menuContext = useContext(MenuContext);
+
+  const Component = disableAnimation ? as || "ul" : motion(as || "ul");
+
+  return (
+    <nav>
+      {menuContext?.isOpen && (
+        <Component
+          role="menu"
+          animate={!disableAnimation ? "animate" : undefined}
+          exit={!disableAnimation ? "exit" : undefined}
+          initial={!disableAnimation ? "initial" : undefined}
+          variants={!disableAnimation ? defaultAnimationVariants : undefined}
+          {...rest}
+        >
+          {children}
+        </Component>
+      )}
+    </nav>
+  );
+});
