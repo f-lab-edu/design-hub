1f1816811c441c3a5e7a5f4ff9cbb8f0f6794f3f
hyungji <icthelight@naver.com>
2024-02-25 14:15:07 +0900

    🎨 chore: 파일 내보내기

--- packages/hj-design-system/components/index.tsx
+++ packages/hj-design-system/components/index.tsx
@@ -2,3 +2,4 @@ export * from "./button";
 export * from "./input";
 export * from "./modal";
 export * from "./portal";
+export * from "./menu";
---
+++ packages/hj-design-system/components/menu/index.tsx
@@ -0,0 +1,7 @@
+import { MenuRoot as Root } from "./menu-root";
+import { MenuTrigger as Trigger } from "./menu-trigger";
+
+export const Menu = {
+  Root,
+  Trigger,
+};
