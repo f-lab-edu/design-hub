4ba490efc1b8a5891a353aab3618f14692f90aa0
hyungji <icthelight@naver.com>
2024-02-25 20:22:03 +0900

    ✨ feat: menu-trigger 외부 영역을 클릭하면 menu-list 숨기기

--- packages/hj-design-system/components/menu/menu-trigger.tsx
+++ packages/hj-design-system/components/menu/menu-trigger.tsx
@@ -4,9 +4,10 @@ import {
   PolymorphicComponentPropsWithRef,
   PolymorphicRef,
 } from "components/polymorphic";
-import { ElementType, forwardRef, useContext, useMemo } from "react";
+import { ElementType, forwardRef, useContext, useMemo, useRef } from "react";
 import { MenuContext } from "./menu-context";
 import { triggerBaseStyle } from "./styles/menu-trigger";
+import { useClickOutSide } from "../../hooks/use-click-outside";
 
 type MenuTriggerProps<C extends ElementType = "button"> =
   PolymorphicComponentPropsWithRef<C>;
@@ -17,6 +18,8 @@ export const MenuTrigger = forwardRef(function MenuTrigger<
   const { as, children, onClick, style, ...rest } = props;
 
   const menuContext = useContext(MenuContext);
+
+  const menuTriggerRef = ref ? ref : useRef<HTMLButtonElement>(null);
 
   const handleClick = () => {
     onClick?.();
@@ -29,13 +32,23 @@ export const MenuTrigger = forwardRef(function MenuTrigger<
     return style ? [triggerBaseStyle, style] : triggerBaseStyle;
   }, [style, triggerBaseStyle]);
 
+  useClickOutSide({
+    targetRef: menuTriggerRef,
+    onClickOutside: () => menuContext?.toggle(),
+  });
+
   if (!menuContext) {
     throw new Error("MenuTrigger should be used within a MenuRoot");
   }
 
   const Component = as || "button";
   return (
-    <Component ref={ref} onClick={handleClick} css={combinedStyle} {...rest}>
+    <Component
+      ref={menuTriggerRef}
+      onClick={handleClick}
+      css={combinedStyle}
+      {...rest}
+    >
       {children}
     </Component>
   );
