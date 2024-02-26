8657db7f48e95cae5fc9ac6e80078a819626f92e
hyungji <icthelight@naver.com>
2024-02-26 21:42:42 +0900

    ✨ feat: menu-item 스타일 추가

--- packages/hj-design-system/components/menu/menu-item.tsx
+++ packages/hj-design-system/components/menu/menu-item.tsx
@@ -6,7 +6,7 @@ import {
 } from "components/polymorphic";
 import { ElementType, forwardRef, useContext, useMemo } from "react";
 import { MenuContext } from "./menu-context";
-import { itemBaseStyles } from "./styles/menu-item";
+import { isSelectedStyle, itemBaseStyles } from "./styles/menu-item";
 
 type MenuItemProps<C extends ElementType = "li"> =
   PolymorphicComponentPropsWithRef<C>;
@@ -14,22 +14,24 @@ type MenuItemProps<C extends ElementType = "li"> =
 export const MenuItem = forwardRef(function MenuItem<
   C extends ElementType = "li",
 >(props: MenuItemProps<C>, ref?: PolymorphicRef<C>) {
-  const { as, children, style, ...rest } = props;
+  const { as, children, style, isSelected, onClick, ...rest } = props;
 
   const menuContext = useContext(MenuContext);
 
   const Component = as || "li";
 
   const combinedStyle = useMemo(() => {
-    return style ? [itemBaseStyles, style] : itemBaseStyles;
-  }, [style, itemBaseStyles]);
+    return style
+      ? [itemBaseStyles, isSelectedStyle(isSelected), style]
+      : [itemBaseStyles, isSelectedStyle(isSelected)];
+  }, [style, itemBaseStyles, isSelected]);
 
   if (!menuContext) {
     throw new Error("MenuItem should be used within a MenuRoot");
   }
 
   return (
-    <Component ref={ref} css={combinedStyle} {...rest}>
+    <Component ref={ref} css={combinedStyle} onClick={onClick} {...rest}>
       {children}
     </Component>
   );
