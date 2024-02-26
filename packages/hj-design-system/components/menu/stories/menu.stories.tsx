9622d077c3583457ac7821afe4fe61a63eb255ea
hyungji <icthelight@naver.com>
2024-02-26 21:52:45 +0900

    📝 docs: menu 스토리 추가

---
+++ packages/hj-design-system/components/menu/stories/menu.stories.tsx
@@ -0,0 +1,22 @@
+import React, { ComponentProps } from "react";
+import { Menu } from "../index";
+
+export default {
+  title: "Components/Menu",
+  component: Menu.Root,
+};
+
+type Args = ComponentProps<typeof Menu.Root>;
+
+const Template = (args: Args) => (
+  <Menu.Root {...args}>
+    <Menu.Trigger>Toggle Menu</Menu.Trigger>
+    <Menu.List>
+      <Menu.Item>Menu Item 1</Menu.Item>
+      <Menu.Item>Menu Item 2</Menu.Item>
+      <Menu.Item>Menu Item 3</Menu.Item>
+    </Menu.List>
+  </Menu.Root>
+);
+
+export const Default = Template.bind({});
