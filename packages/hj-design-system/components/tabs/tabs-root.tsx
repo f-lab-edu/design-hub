9718d16791e265bfe5b70264e763f78dce60a7ec
hyungji <icthelight@naver.com>
2024-03-03 16:32:14 +0900

    ✨ feat: tabs-root 를 div 로 제한하기

--- packages/hj-design-system/components/tabs/tabs-root.tsx
+++ packages/hj-design-system/components/tabs/tabs-root.tsx
@@ -1,60 +1,45 @@
-import {
-  PolymorphicComponentPropsWithRef,
-  PolymorphicRef,
-} from "components/polymorphic";
-import { ElementType, forwardRef } from "react";
+import { ForwardedRef, HTMLAttributes, forwardRef } from "react";
 
 type TabsSizeSet = "sm" | "md" | "lg";
 
 type TabsDirection = "horizontal" | "vertical";
 
-type TabsRootProps<C extends ElementType = "div"> =
-  PolymorphicComponentPropsWithRef<
-    C,
-    {
-      /**
-       * The size of the tabs.
-       * @default "md"
-       */
-      size?: TabsSizeSet;
-      /**
-       * The current selected tab index.
-       * @default 0
-       */
-      current?: number;
-      /**
-       * The callback function that is called when the tab is clicked.
-       */
-      onChange?: (index: number) => void;
-      /**
-       * Default tab index.
-       * @default 0
-       */
-      defaultIndex?: number;
-      /**
-       * The direction of the tabs.
-       * @default "horizontal"
-       */
-      direction?: TabsDirection;
-    }
-  >;
+type TabsRootProps = HTMLAttributes<HTMLDivElement> & {
+  /**
+   * The size of the tabs.
+   * @default "md"
+   */
+  size?: TabsSizeSet;
+  /**
+   * The current selected tab index.
+   * @default 0
+   */
+  current?: number;
+  /**
+   * The callback function that is called when the tab is clicked.
+   */
+  onChangeCurrent?: (index: number) => void;
+  /**
+   * Default tab index.
+   * @default 0
+   */
+  defaultIndex?: number;
+  /**
+   * The direction of the tabs.
+   * @default "horizontal"
+   */
+  direction?: TabsDirection;
+};
 
-export const TabsRoot = forwardRef(function TabsRoot<
-  C extends ElementType = "div",
->(props: TabsRootProps<C>, ref?: PolymorphicRef<C>) {
-  const {
-    as,
-    children,
-    size = "md",
-    direction = "horizontal",
-    ...rest
-  } = props;
-
-  const Component = as || "div";
+export const TabsRoot = forwardRef(function TabsRoot(
+  props: TabsRootProps,
+  ref?: ForwardedRef<HTMLDivElement>
+) {
+  const { children, size = "md", direction = "horizontal", ...rest } = props;
 
   return (
-    <Component ref={ref} {...rest}>
+    <div ref={ref} {...rest}>
       {children}
-    </Component>
+    </div>
   );
 });
