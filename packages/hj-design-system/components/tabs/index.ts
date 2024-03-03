8a6f373f11d5cb15f2e5d7672390a792f8295698
hyungji <icthelight@naver.com>
2024-03-03 21:09:50 +0900

    🎨 chore: tabs-panels, tabs-panel 임포트

--- packages/hj-design-system/components/tabs/index.ts
+++ packages/hj-design-system/components/tabs/index.ts
@@ -2,10 +2,14 @@ import {TabsTitle as Title} from './tabs-title';
 import {TabsRoot as Root} from './tabs-root';
 import {TabsList as List} from './tabs-list';
 import {TabsTab as Tab} from './tabs-tab';
+import { TabsPanels as Panels } from './tabs-panels';
+import {TabsPanel as Panel} from './tabs-panel';
 
 export const Tabs = {
     Root,
     Title,
     List,
-    Tab
+    Tab,
+    Panels,
+    Panel
 }
