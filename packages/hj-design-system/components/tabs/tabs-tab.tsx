fbdb84ab92db79ad201f3d292eb9c36ab226e569
hyungji <icthelight@naver.com>
2024-03-03 19:30:28 +0900

    ✨ feat: tabs-tab 에 프로바이더를 감싸지 않으면 에러 뱉기

--- packages/hj-design-system/components/tabs/tabs-tab.tsx
+++ packages/hj-design-system/components/tabs/tabs-tab.tsx
@@ -2,8 +2,8 @@ import {
   PolymorphicComponentPropsWithRef,
   PolymorphicRef,
 } from "components/polymorphic";
-import { ComponentProps, ElementType, forwardRef } from "react";
-import { TabsProvider } from "./tabs-context";
+import { ComponentProps, ElementType, forwardRef, useContext } from "react";
+import { TabsContext, TabsProvider } from "./tabs-context";
 
 type TabChangeHandler = ComponentProps<typeof TabsProvider>["onChangeCurrent"];
 
@@ -21,7 +21,13 @@ export const TabsTab = forwardRef(function TabsTab<
 >(props: TabsTabProps<C>, ref?: PolymorphicRef<C>) {
   const { as, handleChange, index, onClick, ...rest } = props;
 
+  const tabsContext = useContext(TabsContext);
+
   const Component = as || "button";
+
+  if (!tabsContext) {
+    throw new Error("Tabs.Tab must be rendered within a Tabs.Root.");
+  }
 
   return (
     <Component
