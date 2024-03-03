afba6a7768e5b39b4cc5c27a8b07fbf9dd973827
hyungji <icthelight@naver.com>
2024-03-03 19:47:47 +0900

    ✨ feat: tabs-tab 은 button 이다

--- packages/hj-design-system/components/tabs/tabs-tab.tsx
+++ packages/hj-design-system/components/tabs/tabs-tab.tsx
@@ -1,40 +1,39 @@
 import {
-  PolymorphicComponentPropsWithRef,
-  PolymorphicRef,
-} from "components/polymorphic";
-import { ComponentProps, ElementType, forwardRef, useContext } from "react";
+  ComponentProps,
+  ForwardedRef,
+  HTMLAttributes,
+  forwardRef,
+  useContext,
+} from "react";
 import { TabsContext, TabsProvider } from "./tabs-context";
 
 type TabChangeHandler = ComponentProps<typeof TabsProvider>["onChangeCurrent"];
 
-type TabsTabProps<C extends ElementType = "button"> =
-  PolymorphicComponentPropsWithRef<
-    C,
-    {
-      handleChange?: TabChangeHandler;
-      index: number;
-    }
-  >;
+type TabsTabProps = HTMLAttributes<HTMLButtonElement> & {
+  handleChange?: TabChangeHandler;
+  index: number;
+};
 
-export const TabsTab = forwardRef(function TabsTab<
-  C extends ElementType = "button",
->(props: TabsTabProps<C>, ref?: PolymorphicRef<C>) {
-  const { as, handleChange, index, onClick, ...rest } = props;
+export const TabsTab = forwardRef(function TabsTab(
+  props: TabsTabProps,
+  ref?: ForwardedRef<HTMLButtonElement>
+) {
+  const { handleChange, index, onClick, ...rest } = props;
 
   const tabsContext = useContext(TabsContext);
-
-  const Component = as || "button";
 
   if (!tabsContext) {
     throw new Error("Tabs.Tab must be rendered within a Tabs.Root.");
   }
 
   return (
-    <Component
+    <button
+      role="tab"
+      aria-selected={tabsContext?.current === index}
       ref={ref}
-      onClick={() => {
+      onClick={(e) => {
         handleChange?.(index);
-        onClick?.();
+        onClick?.(e);
       }}
       {...rest}
     />
