e6c8ba3f66fa6d520f20ea8d3dea290af3b764ce
hyungji <icthelight@naver.com>
2024-03-03 19:20:37 +0900

    🎨 chore: tabs-list, tabs-tab 임포트

--- packages/hj-design-system/components/tabs/index.ts
+++ packages/hj-design-system/components/tabs/index.ts
@@ -1,7 +1,11 @@
 import {TabsTitle as Title} from './tabs-title';
 import {TabsRoot as Root} from './tabs-root';
+import {TabsList as List} from './tabs-list';
+import {TabsTab as Tab} from './tabs-tab';
 
 export const Tabs = {
     Root,
-    Title
+    Title,
+    List,
+    Tab
 }
