3442d6f03d6834881db31f15b5c420834dcd3dec
hyungji <icthelight@naver.com>
2024-03-04 20:46:45 +0900

    ✅ test: tabs-list 키보드 인터렉션 테스트 추가

--- packages/hj-design-system/components/tabs/__test__/tabs-list.test.tsx
+++ packages/hj-design-system/components/tabs/__test__/tabs-list.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { TabsList } from "../tabs-list";
 import { TabsProvider } from "../tabs-context";
 import { describe, it, expect } from "vitest";
@@ -8,9 +8,9 @@ describe("TabsList", () => {
     render(
       <TabsProvider>
         <TabsList>
-          <div>Tab 1</div>
-          <div>Tab 2</div>
-          <div>Tab 3</div>
+          <button>Tab 1</button>
+          <button>Tab 2</button>
+          <button>Tab 3</button>
         </TabsList>
       </TabsProvider>
     );
@@ -18,6 +18,67 @@ describe("TabsList", () => {
     expect(screen.getByText("Tab 1")).toBeInTheDocument();
     expect(screen.getByText("Tab 2")).toBeInTheDocument();
     expect(screen.getByText("Tab 3")).toBeInTheDocument();
+  });
+
+  it('키보드 이벤트를 받아서 "ArrowRight" 이면 다음 탭으로 이동한다', () => {
+    render(
+      <TabsProvider>
+        <TabsList>
+          <button>Tab 1</button>
+          <button>Tab 2</button>
+          <button>Tab 3</button>
+        </TabsList>
+      </TabsProvider>
+    );
+
+    const tab1 = screen.getByText("Tab 1");
+    const tab2 = screen.getByText("Tab 2");
+    const tab3 = screen.getByText("Tab 3");
+
+    tab1.focus();
+    fireEvent.keyDown(tab1, { key: "ArrowRight" });
+    expect(tab2).toHaveFocus();
+
+    fireEvent.keyDown(tab2, { key: "ArrowRight" });
+    expect(tab3).toHaveFocus();
+  });
+
+  it('키보드 이벤트를 받아서 "ArrowLeft" 이면 이전 탭으로 이동한다', () => {
+    render(
+      <TabsProvider>
+        <TabsList>
+          <button>Tab 1</button>
+          <button>Tab 2</button>
+          <button>Tab 3</button>
+        </TabsList>
+      </TabsProvider>
+    );
+
+    const tab1 = screen.getByText("Tab 1");
+    const tab3 = screen.getByText("Tab 3");
+
+    tab1.focus();
+    fireEvent.keyDown(tab1, { key: "ArrowLeft" });
+    expect(tab3).toHaveFocus();
+  });
+
+  it("비활성화된 탭은 키보드 이벤트를 받지 않는다", () => {
+    render(
+      <TabsProvider>
+        <TabsList>
+          <button>Tab 1</button>
+          <button disabled>Tab 2</button>
+          <button>Tab 3</button>
+        </TabsList>
+      </TabsProvider>
+    );
+
+    const tab1 = screen.getByText("Tab 1");
+    const tab3 = screen.getByText("Tab 3");
+
+    tab1.focus();
+    fireEvent.keyDown(tab1, { key: "ArrowRight" });
+    expect(tab3).toHaveFocus();
   });
 
   it("TabsProvider로 감싸지 않으면 에러를 뱉는다", () => {
