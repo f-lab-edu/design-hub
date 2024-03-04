a1e755f4f306327e7de871003ebafd4dedd43a70
hyungji <icthelight@naver.com>
2024-03-04 21:25:58 +0900

    ✅ test: tabs-list arrow-down arrow-up 테스트 추가

--- packages/hj-design-system/components/tabs/__test__/tabs-list.test.tsx
+++ packages/hj-design-system/components/tabs/__test__/tabs-list.test.tsx
@@ -81,6 +81,48 @@ describe("TabsList", () => {
     expect(tab3).toHaveFocus();
   });
 
+  it('키보드 이벤트를 받아서 "ArrowDown" 이면 다음 탭으로 이동한다', () => {
+    render(
+      <TabsProvider>
+        <TabsList>
+          <button>Tab 1</button>
+          <button>Tab 2</button>
+          <button>Tab 3</button>
+        </TabsList>
+      </TabsProvider>
+    );
+
+    const tab1 = screen.getByText("Tab 1");
+    const tab2 = screen.getByText("Tab 2");
+    const tab3 = screen.getByText("Tab 3");
+
+    tab1.focus();
+    fireEvent.keyDown(tab1, { key: "ArrowDown" });
+    expect(tab2).toHaveFocus();
+
+    fireEvent.keyDown(tab2, { key: "ArrowDown" });
+    expect(tab3).toHaveFocus();
+  });
+
+  it('키보드 이벤트를 받아서 "ArrowUp" 이면 이전 탭으로 이동한다', () => {
+    render(
+      <TabsProvider>
+        <TabsList>
+          <button>Tab 1</button>
+          <button>Tab 2</button>
+          <button>Tab 3</button>
+        </TabsList>
+      </TabsProvider>
+    );
+
+    const tab1 = screen.getByText("Tab 1");
+    const tab3 = screen.getByText("Tab 3");
+
+    tab1.focus();
+    fireEvent.keyDown(tab1, { key: "ArrowUp" });
+    expect(tab3).toHaveFocus();
+  });
+
   it("TabsProvider로 감싸지 않으면 에러를 뱉는다", () => {
     expect(() => {
       render(<TabsList />);
