57f60a70f67b08d40c7e45019de23cf30f18c046
hyungji <icthelight@naver.com>
2024-03-14 21:01:43 +0900

    ✨ feat: combobox-input 생성

---
+++ packages/hj-design-system/components/combobox/combobox-input.tsx
@@ -0,0 +1,26 @@
+import { ForwardedRef, HTMLAttributes, forwardRef, useContext } from "react";
+import { ComboboxContext } from "./combobox-context";
+
+type ComboboxInput = HTMLAttributes<HTMLInputElement>;
+
+export const ComboboxInput = forwardRef(function ComboboxInput(
+  props: ComboboxInput,
+  ref: ForwardedRef<HTMLInputElement>
+) {
+  const comboboxContext = useContext(ComboboxContext);
+
+  if (!comboboxContext) {
+    throw new Error("Combobox.Input should be used within a Combobox.Root");
+  }
+
+  return (
+    <input
+      {...props}
+      ref={ref}
+      role="combobox"
+      onFocus={() => comboboxContext.changeIsOpen(true)}
+      onBlur={() => comboboxContext.changeIsOpen(false)}
+      aria-expanded={comboboxContext.isOpen}
+    />
+  );
+});
