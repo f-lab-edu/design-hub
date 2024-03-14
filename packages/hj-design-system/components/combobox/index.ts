e5200e62cf6d3f1dfe79cf3239289220dba460f5
hyungji <icthelight@naver.com>
2024-03-14 21:25:31 +0900

    ✨ feat: combobox-list 내보내기

--- packages/hj-design-system/components/combobox/index.ts
+++ packages/hj-design-system/components/combobox/index.ts
@@ -1,7 +1,9 @@
 import {ComboboxRoot as Root} from './combobox-root';
 import {ComboboxInput as Input} from './combobox-input';
+import {ComboboxList as List} from './combobox-list'
 
 export const Combobox = {
     Root,
     Input,
+    List
 };
