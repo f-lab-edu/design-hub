77d6b20d761a47c4f7e56574b86f55cd828c0113
hyungji <icthelight@naver.com>
2024-03-14 21:41:13 +0900

    ✨ feat: combobox-item 내보내기

--- packages/hj-design-system/components/combobox/index.ts
+++ packages/hj-design-system/components/combobox/index.ts
@@ -1,9 +1,11 @@
 import {ComboboxRoot as Root} from './combobox-root';
 import {ComboboxInput as Input} from './combobox-input';
 import {ComboboxList as List} from './combobox-list'
+import {ComboboxItem as Item} from './combobox-item'
 
 export const Combobox = {
     Root,
     Input,
-    List
+    List,
+    Item
 };
