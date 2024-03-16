defb5fb0f715c8e86bc98d0bf04e82f8730382da
hyungji <icthelight@naver.com>
2024-03-16 16:48:45 +0900

    💄 feat: combobox-input 스타일 추가

--- packages/hj-design-system/components/combobox/styles/combobox-input.ts
+++ packages/hj-design-system/components/combobox/styles/combobox-input.ts
@@ -2,6 +2,7 @@ import { css } from "@emotion/react";
 import { foundations } from "../../../theme/foundations";
 
 export const inputBaseStyle = css({
+    position: 'relative',
     padding: foundations.sizes["2.5"],
     borderRadius: foundations.radii.md,
     border: `1px solid ${foundations.colors.gray[300]}`,
