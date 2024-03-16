86ff598a6c2a7ebdb15dae46411b340c7db3b42a
hyungji <icthelight@naver.com>
2024-03-16 16:48:56 +0900

    💄 feat: combobox-list 스타일 추가

--- packages/hj-design-system/components/combobox/combobox-list.tsx
+++ packages/hj-design-system/components/combobox/combobox-list.tsx
@@ -1,3 +1,5 @@
+/** @jsxImportSource @emotion/react */
+
 import {
   PolymorphicComponentPropsWithRef,
   PolymorphicRef,
@@ -10,10 +12,12 @@ import {
   forwardRef,
   isValidElement,
   useContext,
+  useMemo,
 } from "react";
 import { ComboboxContext } from "./combobox-context";
 import { AnimatePresenceProps, motion } from "framer-motion";
 import { defaultAnimationVariants } from "../../theme/foundations/animation";
+import { listBaseStyle } from "./styles/combobox-list";
 
 type ComboboxListProps<C extends ElementType = "ul"> =
   PolymorphicComponentPropsWithRef<
@@ -34,12 +38,22 @@ type ComboboxListProps<C extends ElementType = "ul"> =
 export const ComboboxList = forwardRef(function ComboboxList<
   C extends ElementType = "ul",
 >(props: ComboboxListProps<C>, ref: PolymorphicRef<C>) {
-  const { as, children, disableAnimation, animatePresenceProps, ...rest } =
-    props;
+  const {
+    as,
+    children,
+    disableAnimation,
+    animatePresenceProps,
+    style,
+    ...rest
+  } = props;
 
   const comboboxContext = useContext(ComboboxContext);
 
   const Copmonent = disableAnimation ? as || "ul" : motion(as || "ul");
+
+  const styles = useMemo(() => {
+    return style ? [listBaseStyle, style] : listBaseStyle;
+  }, [listBaseStyle, style]);
 
   if (!comboboxContext) {
     throw new Error("Combobox.List should be used within a Combobox.Root");
@@ -54,6 +68,7 @@ export const ComboboxList = forwardRef(function ComboboxList<
           animate={!disableAnimation ? "animate" : undefined}
           variants={!disableAnimation ? defaultAnimationVariants : undefined}
           tabIndex={0}
+          css={styles}
           {...rest}
         >
           {Children.map(children, (child) => {
