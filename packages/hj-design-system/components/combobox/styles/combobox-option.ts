584f96dab2c07e866ef9f7acc246c05d48e8700b
hyungji <icthelight@naver.com>
2024-03-16 16:49:11 +0900

    💄 feat: combobox-option 스타일 추가

--- packages/hj-design-system/components/combobox/styles/combobox-option.ts
+++ packages/hj-design-system/components/combobox/styles/combobox-option.ts
@@ -2,6 +2,11 @@ import { css } from "@emotion/react";
 import { foundations } from "../../../theme/foundations";
 
 export const optionBaseStyle = css({
+    padding: foundations.space[2],  
     fontSize: foundations.fontSizes.md,
     color: foundations.colors.black,
+    borderRadius: foundations.radii.md,
+    ":hover": {
+        background: foundations.colors.blue[200],
+    },
 })
