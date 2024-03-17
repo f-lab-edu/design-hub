d60e2fdcade279d52ed1a0c78ad2be73e51d23cb
hyungji <icthelight@naver.com>
2024-03-17 21:17:47 +0900

    ✨ feat: combobox-input enter 인터렉션 추가

--- packages/hj-design-system/components/combobox/combobox-input.tsx
+++ packages/hj-design-system/components/combobox/combobox-input.tsx
@@ -26,18 +26,23 @@ export const ComboboxInput = forwardRef(function ComboboxInput(
     if (!comboboxContext) return;
     const inputTarget = e.target as HTMLInputElement;
     if (!inputTarget.nextSibling) return;
-    const optionsLength = (inputTarget.nextSibling as HTMLElement).children
-      .length;
+    const options = (inputTarget.nextSibling as HTMLElement).children;
 
     if (e.key === "ArrowDown") {
-      const nextIndex = (comboboxContext.currentIndex + 1) % optionsLength;
+      const nextIndex = (comboboxContext.currentIndex + 1) % options.length;
       comboboxContext?.changeCurrentIndex(nextIndex);
     } else if (e.key === "ArrowUp") {
       if (comboboxContext.currentIndex === 0) {
-        comboboxContext?.changeCurrentIndex(optionsLength - 1);
+        comboboxContext?.changeCurrentIndex(options.length - 1);
       } else {
         comboboxContext?.changeCurrentIndex(comboboxContext.currentIndex - 1);
       }
+    } else if (e.key === "Enter") {
+      e.preventDefault();
+      comboboxContext?.changeCurrent(
+        (options[comboboxContext.currentIndex] as HTMLElement).outerText
+      );
+      comboboxContext?.changeIsOpen(false);
     }
   };
 
