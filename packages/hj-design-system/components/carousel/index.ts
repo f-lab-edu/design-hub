e3c8855286a6851dfd9095ee2e527a3199b975b3
hyungji <icthelight@naver.com>
2024-03-23 17:18:31 +0900

    🎨  chore : carousel-indicator-group 내보내기

--- packages/hj-design-system/components/carousel/index.ts
+++ packages/hj-design-system/components/carousel/index.ts
@@ -3,11 +3,13 @@ import { CarouselControl as Control } from './carousel-control';
 import {CarouselTrigger as Trigger} from './carousel-trigger';
 import { CarouselItemGroup as ItemGroup } from './carousel-item-group';
 import {CarouselItem as Item} from './carousel-item';
+import {CarouselIndicatorGroup as IndicatorGroup} from './carousel-indicator-group';
 
 export const Carousel = {
     Root,
     Control,
     Trigger,
     ItemGroup,
-    Item
+    Item,
+    IndicatorGroup
 }
