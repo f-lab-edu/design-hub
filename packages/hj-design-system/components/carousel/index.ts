36f8bfe4cf742b3c047c53c76d357336b5ef162e
hyungji <icthelight@naver.com>
2024-03-23 17:02:52 +0900

    🎨 chore: carousel-item-group 내보내기

--- packages/hj-design-system/components/carousel/index.ts
+++ packages/hj-design-system/components/carousel/index.ts
@@ -1,9 +1,11 @@
 import {CarouselRoot as Root} from './carousel-root';
 import { CarouselControl as Control } from './carousel-control';
 import {CarouselTrigger as Trigger} from './carousel-trigger';
+import { CarouselItemGroup as ItemGroup } from './carousel-item-group';
 
 export const Carousel = {
     Root,
     Control,
-    Trigger
+    Trigger,
+    ItemGroup
 }
