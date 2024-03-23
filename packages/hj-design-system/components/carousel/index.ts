c68e7513d14a15e35f62c70a5770ddec84872008
hyungji <icthelight@naver.com>
2024-03-23 16:50:18 +0900

    🎨 chore: carousel-trigger 내보내기

--- packages/hj-design-system/components/carousel/index.ts
+++ packages/hj-design-system/components/carousel/index.ts
@@ -1,7 +1,9 @@
 import {CarouselRoot as Root} from './carousel-root';
 import { CarouselControl as Control } from './carousel-control';
+import {CarouselTrigger as Trigger} from './carousel-trigger';
 
 export const Carousel = {
     Root,
-    Control
+    Control,
+    Trigger
 }
