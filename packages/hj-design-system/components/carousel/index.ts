e327d8412103fdd50c67fad9d2396f5b426b3ff6
hyungji <icthelight@naver.com>
2024-03-23 16:46:07 +0900

    🎨 chore: carousel-control 내보내기

--- packages/hj-design-system/components/carousel/index.ts
+++ packages/hj-design-system/components/carousel/index.ts
@@ -1,5 +1,7 @@
 import {CarouselRoot as Root} from './carousel-root';
+import { CarouselControl as Control } from './carousel-control';
 
 export const Carousel = {
-    Root
+    Root,
+    Control
 }
