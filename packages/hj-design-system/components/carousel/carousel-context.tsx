179645ad23134059589b717adb047bf2783821aa
hyungji <icthelight@naver.com>
2024-03-23 16:57:47 +0900

    ✨ feat: 호출하는쪽에서 context 사용에 집중하도록 컨텍스트 관련 로직 캡슐화

--- packages/hj-design-system/components/carousel/carousel-context.tsx
+++ packages/hj-design-system/components/carousel/carousel-context.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, createContext, useMemo } from "react";
+import { ReactNode, createContext, useContext, useMemo } from "react";
 
 interface CarouselContext {
   /**
@@ -25,4 +25,16 @@ export const CarouselProvider = ({ children, current }: ContextProvider) => {
       {children}
     </CarouselContext.Provider>
   );
+};
+
+export const useCarousel = () => {
+  const context = useContext(CarouselContext);
+
+  if (!context) {
+    throw new Error(
+      "useCarousel must be used within a CarouselProvider. Did you forget to wrap your component in a Carousel.Root?"
+    );
+  }
+
+  return context;
 };
