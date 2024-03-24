895eaac08f37b840d6ce47bbafcea5f2d06bb44b
hyungji <icthelight@naver.com>
2024-03-24 15:59:41 +0900

    💄 feat: carousel-root 스타일 추가

--- packages/hj-design-system/components/carousel/carousel-root.tsx
+++ packages/hj-design-system/components/carousel/carousel-root.tsx
@@ -1,5 +1,9 @@
-import { ForwardedRef, HTMLAttributes, forwardRef } from "react";
+/** @jsxImportSource @emotion/react */
+
+import { ForwardedRef, HTMLAttributes, forwardRef, useMemo } from "react";
 import { CarouselProvider } from "./carousel-context";
+import { rootBaseStyle } from "./style/carousel-root";
+import { css } from "@emotion/react";
 
 type CarouselRootProps = HTMLAttributes<HTMLDivElement> & {
   /**
@@ -12,11 +16,16 @@ export const CarouselRoot = forwardRef(function CarouselRoot(
   props: CarouselRootProps,
   ref?: ForwardedRef<HTMLDivElement>
 ) {
-  const { children, current, ...rest } = props;
+  const { children, current, style, ...rest } = props;
+
+  const styles = useMemo(() => {
+    if (style) return css(rootBaseStyle, { ...style });
+    return rootBaseStyle;
+  }, [style]);
 
   return (
     <CarouselProvider current={current}>
-      <div ref={ref} aria-roledescription="carousel" {...rest}>
+      <div ref={ref} aria-roledescription="carousel" css={styles} {...rest}>
         {children}
       </div>
     </CarouselProvider>
---
+++ packages/hj-design-system/components/carousel/style/carousel-root.ts
@@ -0,0 +1,5 @@
+import { css } from "@emotion/react";
+
+export const rootBaseStyle = css({
+    position: 'relative',
+})
